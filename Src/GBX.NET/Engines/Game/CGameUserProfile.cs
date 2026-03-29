e4a7ace590b553e633e2130a9c3967449574b535
Petr Pivoňka <petrpiv1@gmail.com>
2026-03-29 17:36:39 +0200

    Add many CGameUserProfile chunks

--- Src/GBX.NET/Engines/Game/CGameUserProfile.cs
+++ Src/GBX.NET/Engines/Game/CGameUserProfile.cs
@@ -20,4 +20,37 @@ public partial class CGameUserProfile
             w.WriteEncapsulated(U01);
         }
     }
+
+    public partial class Chunk031CC009 : IVersionable
+    {
+        public int Version { get; set; }
+
+        public Id U01;
+
+        public override void ReadWrite(CGameUserProfile n, GbxReaderWriter rw)
+        {
+            rw.VersionInt32(this);
+            rw.Id(ref U01);
+        }
+    }
+
+    public partial class Chunk031CC01B
+    {
+        public (int, int, int, int, int, int, int, int)[]? U01;
+
+        public override void ReadWrite(CGameUserProfile n, GbxReaderWriter rw)
+        {
+            rw.Array<(int, int, int, int, int, int, int, int)>(ref U01, length: n.unknowns5?.Length ?? 0);
+        }
+    }
+
+    public partial class Chunk031CC021
+    {
+        public int[]? U01;
+
+        public override void ReadWrite(CGameUserProfile n, GbxReaderWriter rw)
+        {
+            rw.Array<int>(ref U01, length: n.unknowns5?.Length ?? 0);
+        }
+    }
 }
