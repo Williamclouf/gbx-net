9eeb6cae0a072a77c4fa9265765cfb75f649139f
Petr Pivoňka <petrpiv1@gmail.com>
2026-03-29 03:35:20 +0200

    Implement some CGameUserProfile chunks

---
+++ Src/GBX.NET/Engines/Game/CGameUserProfile.cs
@@ -0,0 +1,23 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameUserProfile
+{
+    public partial class Chunk031CC001 : IVersionable
+    {
+        public int Version { get; set; }
+
+        public RawData U01 = new([], null);
+
+        public override void Read(CGameUserProfile n, GbxReader r)
+        {
+            Version = r.ReadInt32();
+            U01 = r.ReadEncapsulated();
+        }
+
+        public override void Write(CGameUserProfile n, GbxWriter w)
+        {
+            w.Write(Version);
+            w.WriteEncapsulated(U01);
+        }
+    }
+}
